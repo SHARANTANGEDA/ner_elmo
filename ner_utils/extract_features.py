b1e9561c93f8f32996504bbfef132e23eabf8193
Sai Sharan Tangeda <saisarantangeda@gmail.com>
2021-03-12 16:32:51 +0530

    Fix Pre-processing

--- ner_utils/extract_features.py
+++ ner_utils/extract_features.py
@@ -1,7 +1,7 @@
 import os
 from tqdm import tqdm
 
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+import numpy as np
 
 from ner_utils.pre_process import read_dataset
 import constants as c
@@ -43,7 +43,8 @@ def retrieve_features(data_type, label_list, max_seq_length):
     for (i, label) in enumerate(label_list):
         label_map[label] = i
     tokens_list, label_id_list = convert_to_input(X, y, label_map, max_seq_length)
+    label_id_list = np.array(label_id_list)
     # tokens = pad_sequences(tokens_list, maxlen=max_seq_length, dtype="long", truncating="post", padding="post")
     # labels = pad_sequences(label_id_list, maxlen=max_seq_length, dtype="long", truncating="post", padding="post")
-    
+    label_id_list = label_id_list.reshape(label_id_list.shape[0], label_id_list.shape[1])
     return tokens_list, label_id_list
