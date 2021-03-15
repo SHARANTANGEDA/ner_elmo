4b8641b3f1c89f91ab3ea5255f7394a94a43844c
Sai Sharan Tangeda <saisarantangeda@gmail.com>
2021-03-15 17:58:03 +0530

    Fix Metrics

--- elmo_model/model.py
+++ elmo_model/model.py
@@ -16,6 +16,9 @@ from tensorflow.keras import Input
 from metrics.metrics import macro_f1, get_classification_report, micro_f1, macro_precision, macro_recall
 from ner_utils import extract_features
 import constants as c
+from tensorflow.python.keras.backend import set_session
+from tensorflow.python.keras.models import load_model
+
 
 
 def elmo_embedding_layer(x):
@@ -38,6 +41,12 @@ def elmo_model():
 
 
 def train_test(epochs, epsilon=1e-7, init_lr=2e-5, beta_1=0.9, beta_2=0.999):
+    sess = tf.Session()
+    graph = tf.get_default_graph()
+    
+    # IMPORTANT: models have to be loaded AFTER SETTING THE SESSION for keras!
+    # Otherwise, their weights will be unavailable in the threads after the session there has been set
+    set_session(sess)
     """Create Features & Tokenize"""
     logging.getLogger().setLevel(logging.INFO)
     #
